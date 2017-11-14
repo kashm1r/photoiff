cc4d2bf259353809322d383d4504d5d587a6766c
fanuelcouto99 <fanuel.couto99@gmail.com>
2017-11-13 23:17:38 -0200

    Atualizado arquivos de gerenciamento de usuario

--- src/service/PerfilService.java
+++ src/service/PerfilService.java
@@ -1,6 +1,10 @@
 package service;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import modelo.Perfil;
 
@@ -9,6 +13,22 @@ public class PerfilService extends GenericService<Perfil>{
 	
 	public PerfilService(){
 		super(Perfil.class);
+	}
+	
+	public List<Perfil> obtemPerfilComMenus(){
+		
+		final CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+		final CriteriaQuery<Perfil> cQuery = cb.createQuery(Perfil.class);
+		
+		cQuery.select(cQuery.from(Perfil.class));
+		
+		List<Perfil> perfis = getEntityManager().createQuery(cQuery).getResultList();
+		
+		for (Perfil p : perfis) {
+			p.getMenus().size();
+		}
+		
+		return perfis;
 	}
 	
 }
