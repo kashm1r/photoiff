854e7b7dae56ceb09bed53ae5cbbab7ad1004bec
fanuel <fanuel.couto99@gmail.com>
2017-11-18 17:24:00 -0200

    Atualizado arquivos de gerenciamento de usuario

--- src/service/PerfilService.java
+++ src/service/PerfilService.java
@@ -1,12 +1,18 @@
 package service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.persistence.QueryTimeoutException;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import modelo.Perfil;
+import modelo.Pessoa;
 
 @Stateless
 public class PerfilService extends GenericService<Perfil>{
@@ -29,6 +35,45 @@ public class PerfilService extends GenericService<Perfil>{
 		}
 		
 		return perfis;
+	}
+	
+	
+	public Pessoa validarUsuario(String log, String pass){
+		final CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+		
+		final CriteriaQuery<Pessoa> cquery = cb.createQuery(Pessoa.class);
+		final Root<Pessoa> root = cquery.from(Pessoa.class);
+		final List<Predicate> condicoes = new ArrayList<Predicate>();
+
+		condicoes.add(cb.equal(root.get("usuario").get("login"), log));
+		condicoes.add(cb.equal(root.get("usuario").get("senha"), pass));
+		
+		cquery.select(root).where(condicoes.toArray(new Predicate[]{}));
+		Pessoa pessoa = new Pessoa();
+		try{
+			pessoa = getEntityManager().createQuery(cquery).getSingleResult();
+		} catch (Exception e) {
+			throw new QueryTimeoutException("Usuário ou senha invalido!");
+		}	
+    	
+    	return pessoa;
+	}
+	
+	
+	public List<Pessoa> filtrarPorNome(String filtro){
+		final CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+		
+		final CriteriaQuery<Pessoa> cquery = cb.createQuery(Pessoa.class);
+		final Root<Pessoa> root = cquery.from(Pessoa.class);
+		final List<Predicate> condicoes = new ArrayList<Predicate>();
+
+		Expression<String> path = root.get("nome");
+		condicoes.add(cb.like(path, "%"+filtro+"%"));
+		
+		cquery.select(root).where(condicoes.toArray(new Predicate[]{}));
+		List<Pessoa> pessoas = getEntityManager().createQuery(cquery).getResultList();
+			    	
+    	return pessoas;
 	}
 	
 }
--- src/service/PessoaService.java
+++ src/service/PessoaService.java
@@ -2,7 +2,17 @@ package service;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ejb.Stateless;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import modelo.Menu;
 import modelo.Pessoa;
 
 @Stateless
@@ -11,5 +21,50 @@ public class PessoaService extends GenericService<Pessoa> {
 	public PessoaService(){
 		super(Pessoa.class);
 	}	
+	
+	public Pessoa validarUsuario(String log, String pass){
+		final CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+		
+		final CriteriaQuery<Pessoa> cquery = cb.createQuery(Pessoa.class);
+		final Root<Pessoa> root = cquery.from(Pessoa.class);
+		final List<Predicate> condicoes = new ArrayList<Predicate>();
+
+		condicoes.add(cb.equal(root.get("login"), log));
+		condicoes.add(cb.equal(root.get("senha"), pass));
+		
+		cquery.select(root).where(condicoes.toArray(new Predicate[]{}));
+		Pessoa pessoa = null;
+
+		try{
+			pessoa = getEntityManager().createQuery(cquery).getSingleResult();
+			
+			for(Menu m : pessoa.getPerfil().getMenus()) {
+				m.getItens().size();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+    	
+		
+		
+    	return pessoa;
+	}
+	
+
+	public List<Pessoa> filtrarPorNome(String filtro){
+		final CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+		
+		final CriteriaQuery<Pessoa> cquery = cb.createQuery(Pessoa.class);
+		final Root<Pessoa> root = cquery.from(Pessoa.class);
+		final List<Predicate> condicoes = new ArrayList<Predicate>();
+
+		Expression<String> path = root.get("nome");
+		condicoes.add(cb.like(path, "%"+filtro+"%"));
+		
+		cquery.select(root).where(condicoes.toArray(new Predicate[]{}));
+		List<Pessoa> pessoas = getEntityManager().createQuery(cquery).getResultList();
+			    	
+    	return pessoas;
+	}
 
 }
