4c8ed2e99b14641f4ab42d8af0b4aac23fee15da
fanuel <fanuel.couto99@gmail.com>
2017-12-07 20:45:00 -0200

    PhotoIFF: Adicionado arquivos de configuração de logout

---
+++ src/tools/LoggedInCheck.java
@@ -0,0 +1,54 @@
+package tools;
+
+
+import java.io.IOException;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+import modelo.Menu;
+import modelo.MenuItem;
+import modelo.Pessoa;
+
+
+
+@SuppressWarnings("serial")
+public class LoggedInCheck implements PhaseListener {
+	
+	public PhaseId getPhaseId() {
+		return PhaseId.RESTORE_VIEW;
+	}
+	
+	public void afterPhase(PhaseEvent event) {
+		final FacesContext context = event.getFacesContext();
+		final ExternalContext ec = context.getExternalContext();
+		final String viewid = context.getViewRoot().getViewId();
+	
+		final Pessoa usuario = (Pessoa) ec.getSessionMap().get("usuario");  
+		try {
+			//System.out.println(usuario.getPerfil());
+	
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		 
+		if(!viewid.startsWith("/index") && 
+				   usuario == null) {
+			
+					try {
+						ec.redirect(ec.getRequestContextPath() + "/index.xhtml");
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+			System.out.println("Usuario não esta logado");
+		}
+		
+		
+	}
+
+	public void beforePhase(PhaseEvent event) {}
+
+}
