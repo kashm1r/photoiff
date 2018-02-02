427473efc10368764432159c4deb622838a793bc
fanuel <fanuel.couto99@gmail.com>
2018-02-01 23:42:42 -0200

    PHOTOIFF: criado classes tag e foto

---
+++ src/modelo/Foto.java
@@ -0,0 +1,94 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Foto {
+	
+	@Id @GeneratedValue
+	private Long id;
+	
+	private String url;
+	
+	private Date dataUpload;
+	
+	private Date dataDownload;
+	
+	@ManyToOne
+	private Pessoa pessoa = new Pessoa();
+	
+	@ManyToMany
+	private List<Tag> tags = new ArrayList<Tag>();
+	
+	@ManyToOne
+	private Diretorio diretorio = new Diretorio();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Date getDataUpload() {
+		return dataUpload;
+	}
+
+	public void setDataUpload(Date dataUpload) {
+		this.dataUpload = dataUpload;
+	}
+
+	public Date getDataDownload() {
+		return dataDownload;
+	}
+
+	public void setDataDownload(Date dataDownload) {
+		this.dataDownload = dataDownload;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public List<Tag> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<Tag> tags) {
+		this.tags = tags;
+	}
+
+	public Diretorio getDiretorio() {
+		return diretorio;
+	}
+
+	public void setDiretorio(Diretorio diretorio) {
+		this.diretorio = diretorio;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/modelo/Tag.java
@@ -0,0 +1,34 @@
+package modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Tag {
+	
+	@Id @GeneratedValue
+	private Long id;
+	
+	@Column (nullable = false, unique = true)
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+}
