3446b61a80bd247ab5cdcecfdcd0cb9d000904da
fanuel <fanuel.couto99@gmail.com>
2018-02-12 10:26:32 -0200

    PHOTOIFF: ConsultarDiretoriosBean - Criado funções para remover partições e add

--- src/controle/ConsultaDiretoriosBean.java
+++ src/controle/ConsultaDiretoriosBean.java
@@ -1,27 +1,23 @@
 package controle;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpSession;
 
+import org.primefaces.context.RequestContext;
 import org.primefaces.model.DefaultTreeNode;
 import org.primefaces.model.TreeNode;
 
 import modelo.Diretorio;
-import modelo.Pessoa;
 import service.DiretorioService;
 
-
-
-@SessionScoped
+@ViewScoped
 @ManagedBean
 public class ConsultaDiretoriosBean {
 
@@ -33,16 +29,32 @@ public class ConsultaDiretoriosBean {
 	
 	private TreeNode selectedNode;
 	
-	private String usuarioLogado;
+	private Diretorio diretorio = new Diretorio();
 	
-	
-
-	public String getUsuarioLogado() {
-		return usuarioLogado;
+	private List<Diretorio> listaDiretorio = new ArrayList<Diretorio>();
+		
+	public List<Diretorio> getListaDiretorio() {
+		return listaDiretorio;
 	}
 
-	public void setUsuarioLogado(String usuarioLogado) {
-		this.usuarioLogado = usuarioLogado;
+	public void setListaDiretorio(List<Diretorio> listaDiretorio) {
+		this.listaDiretorio = listaDiretorio;
+	}
+
+	public Diretorio getDiretorio() {
+		return diretorio;
+	}
+
+	public void setDiretorio(Diretorio diretorio) {
+		this.diretorio = diretorio;
+	}
+	
+	public TreeNode getRaiz() {
+		return raiz;
+	}
+
+	public void setRaiz(TreeNode raiz) {
+		this.raiz = raiz;
 	}
 
 	public TreeNode getSelectedNode() {
@@ -55,11 +67,7 @@ public class ConsultaDiretoriosBean {
 
 	@PostConstruct
 	public void consultar() {
-		final ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext(); 
-		final HttpSession session = (HttpSession) ec.getSession(true);
-		Pessoa user = (Pessoa) session.getAttribute("usuario");
-		setUsuarioLogado(user.getNome());
-		
+				
 		List<Diretorio> diretorioRaizes = diretoriosService.raizesComNos();
 
 		raiz = new DefaultTreeNode("Raiz", null);
@@ -77,16 +85,63 @@ public class ConsultaDiretoriosBean {
 		}
 	}
 
-	public TreeNode getRaiz() {
-		return raiz;
-	}
-	
 	public void displaySelectedSingle() {
         if(selectedNode != null) {
             FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Selected", selectedNode.getData().toString());
             FacesContext.getCurrentInstance().addMessage(null, message);
         }
     }
+	
+	public void removerParticao() {
+		if(selectedNode != null) {
+			if(selectedNode.getChildCount() > 0) {
+				 FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Partição Principal", "Favor remover as partições secundárias");
+		         FacesContext.getCurrentInstance().addMessage(null, message);
+			}
+			
+			else {
+				System.out.println(selectedNode.getData().toString());
+				Diretorio dir = diretoriosService.buscarDiretorio(selectedNode.getData().toString());
+				diretoriosService.remove(dir);
+				FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Partição Excluída", selectedNode.getData().toString());
+		        FacesContext.getCurrentInstance().addMessage(null, message);
+			}
+		}
+	}
+	
+	public void adicionarParticao() {
+		if(selectedNode == null) {
+			// Criar uma partição pai, setando o diretorio pai como nulo
+		} else {
+			// Criar uma subpartição através de um objeto já existente, setando o diretório pai igual ao ID do mesmo
+		}
+	}
+	
+	public void editarParticao(Diretorio dir) {
+		setDiretorio(dir);
+		
+	}
+	
+	public void exibirTelaCadastroDiretorio(Diretorio d) {
+		if(getDiretorio().getId() == null) {
+			setDiretorio(new Diretorio());
+			RequestContext.getCurrentInstance().execute("PF('particaoDl').show()");
+		} else {
+			setDiretorio(new Diretorio());
+			RequestContext.getCurrentInstance().execute("PF('particaoDl').show()");
+		}
+		setDiretorio(new Diretorio());
+	}
+	
+	public void fecharTelaEditarDiretorio(Diretorio d) {
+		if(getDiretorio().getId() == null) {
+			setDiretorio(new Diretorio());
+			RequestContext.getCurrentInstance().execute("PF('particaoDl').hide()");
+		} else {
+			RequestContext.getCurrentInstance().execute("PF('particaoDl').hide()");
+		}
+		
+	}
 
 
 }
